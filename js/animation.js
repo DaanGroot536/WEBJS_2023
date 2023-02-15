215b642ab8bfd8216cea2e2b6bcc4e371f813412
DaanMonk <d.groot3@student.avans.nl>
2023-02-15 11:08:42 +0100

    packages move seperately

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,14 @@
-  export function animate(beltcounter) {
-    alert(beltcounter)
+export function animate(beltcounter) {
+    localStorage.setItem('package'+beltcounter, 0);
+    setInterval(move, 10, beltcounter);
+}
+
+function move(beltcounter) {
+    let node = document.getElementById("package"+beltcounter);
+    // console.log(node, currentPosition);
+    let currentPosition = parseInt(localStorage.getItem('package'+beltcounter));
+    currentPosition = currentPosition + 1;
+    localStorage.setItem('package'+beltcounter, currentPosition);
+    console.log(currentPosition);
+    node.style.left = currentPosition + "px";
 }
--- js/beltmaker.js
+++ js/beltmaker.js
@@ -9,12 +9,16 @@ function addBelt() {
         let beltpane = document.getElementById('beltpanel');
         let beltstart = document.createElement('button');
         beltstart.className = "btn btn-secondary col-2 my-auto";
-        beltstart.innerHTML = "Start belt "+(beltcounter+1);
+        beltstart.innerHTML = "Start belt "+(beltcounter);
         beltstart.addEventListener('click', (event) => {
             animate(beltstart.innerHTML.charAt(beltstart.innerHTML.length-1));
         });
         let newBelt = document.createElement('div');
         newBelt.className = "conveyorbelt col-10";
+        let beltItem = document.createElement('div');
+        beltItem.id = "package"+beltcounter;
+        beltItem.className = "package";
+        newBelt.appendChild(beltItem);
 
         let beltRow = document.createElement('div');
         beltRow.className = "row mt-3";
