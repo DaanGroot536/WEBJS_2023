73c961a247f54591c67aeb437e7c4665e48446d7
xpetersen <ptrsnxndr@gmail.com>
2023-02-17 22:04:39 +0100

    added error messages

--- js/form.js
+++ js/form.js
@@ -1,5 +1,8 @@
 var currentTab = 0;
+setupForm();
 showTab(currentTab);
+
+const emailError = document.querySelector("#mail + span.error");
 
 function showTab(n) {
 
@@ -57,6 +60,7 @@ function validateForm() {
 
         // check if empty
         if (y[i].value == "") {
+            emailError.textContent = "You need to enter a value.";
             y[i].className += " invalid";
             valid = false;
             continue;
@@ -64,6 +68,7 @@ function validateForm() {
 
         // for number fields, check if input between 1 and 5
         if (y[i].type = "number" && (y[i].value <= 0 || y[i].value > 5)) {
+            emailError.textContent = "Entered value needs to be between 1 and 5.";
             y[i].className += " invalid";
             valid = false;
         }
@@ -71,7 +76,12 @@ function validateForm() {
 
     // if all fields are valid, update step indicator
     if (valid) {
+        emailError.textContent = "";
+        emailError.className = "error";
+
         document.getElementsByClassName("step")[currentTab].className += " finish";
+    } else {
+        emailError.className = "error active";
     }
 
     return valid;
@@ -83,7 +93,19 @@ function fixStepIndicator(n) {
     for (i = 0; i < x.length; i++) {
         x[i].className = x[i].className.replace(" active", "");
     }
-    
+
     // activate current step
     x[n].className += " active";
+}
+
+function setupForm() {
+    // get all input fields that require validation
+    let inputFields = document.querySelectorAll("input");
+
+    // add an event listener to each input field to validate input
+    inputFields.forEach(function (elem) {
+        elem.addEventListener("input", () => {
+            validateForm();
+        });
+    });
 }
