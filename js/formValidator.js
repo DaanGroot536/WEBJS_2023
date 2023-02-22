ac187b0204980f12d51c35b0861f4af222ed4f4b
xpetersen <ptrsnxndr@gmail.com>
2023-02-22 23:49:45 +0100

    refactored form validator

--- js/formValidator.js
+++ js/formValidator.js
@@ -1,8 +1,9 @@
 export default class FormValidator {
-    constructor () {
-        
+    constructor() {
+
     }
-    
+
+    // only allows numbers to be entered
     onlyNumberKey(evt) {
         var ASCIICode = (evt.which) ? evt.which : evt.keyCode
         if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
@@ -10,49 +11,40 @@ export default class FormValidator {
         return true;
     }
 
+    // validate fields in a given tab
     validateForm(currentTab) {
         let valid = true;
-        let errorMessage;
-    
-        // get current tab
-        const tabs = document.getElementsByClassName("tab");
-    
-        // get all fields in tab
-        let fields = tabs[currentTab].getElementsByTagName("input");
-    
-        for (let i = 0; i < fields.length; i++) {
-    
-            errorMessage = document.getElementById(fields[i].name + "error");
-    
-            // check if empty
-            if (fields[i].value == "") {
-                errorMessage.textContent = "You need to enter a value.";
-                errorMessage.className = "error active";
-                fields[i].className += " invalid";
-                valid = false;
-                continue;
-            }
-    
-            // for number fields, check if input between 1 and 5
-            if (fields[i].type = "number" && (fields[i].value <= 0 || fields[i].value > 5)) {
-                errorMessage.textContent = "Entered value needs to be between 1 and 5.";
-                errorMessage.className = "error active";
-                fields[i].className += " invalid";
-                valid = false;
-                continue;
-            }
-        }
-    
-        // if all fields are valid, update step indicator
-        if (valid) {
-            if(errorMessage) {
-                errorMessage.textContent = "";
-                errorMessage.className = "error";
-            }
 
-            document.getElementsByClassName("step")[currentTab].className += " finish";
+        // get field to validate
+        const tab = document.getElementsByClassName("tab")[currentTab];
+        let fieldToValidate = tab.getElementsByTagName("input")[0];
+
+        if (!fieldToValidate) return valid;
+
+        let errorMessage = document.getElementById(fieldToValidate.name + "error");
+
+        // check if empty
+        if (fieldToValidate.value == "") {
+            errorMessage.textContent = "You need to enter a value.";
+            valid = false;
+        } else if (fieldToValidate.value <= 0 || fieldToValidate.value > 5) {
+            errorMessage.textContent = "Entered value needs to be between 1 and 5.";
+            valid = false;
         }
-    
+
+        if (valid) {
+            // hide error message
+            errorMessage.textContent = "";
+            errorMessage.className = "error";
+
+            // update step indicator
+            document.getElementsByClassName("step")[currentTab].className += " finish";
+        } else {
+            // show error message
+            errorMessage.className = "error active";
+            fieldToValidate.className += " invalid";
+        }
+
         return valid;
     }
 }
