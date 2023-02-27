93ee3c5e67dcbf03ee0eba9e06de865c74925870
xpetersen <ptrsnxndr@gmail.com>
2023-02-27 19:24:23 +0100

    remove logs

--- js/truckmanager.js
+++ js/truckmanager.js
@@ -27,11 +27,6 @@ function startBeltAgain(truckID) {
 
 }
 
-//get weather
-//compare for each truck if it can be on the road
-//args:
-//-strong wind != pallets
-
 export function checkWeather(weatherData) {
     let trucks = [];
 
@@ -39,16 +34,12 @@ export function checkWeather(weatherData) {
         let key = window.localStorage.key(i);
         if (key.slice(0, 5) === "truck") {
             trucks.push(JSON.parse(window.localStorage.getItem(key)));
-            console.log(trucks);
             checkRoadClearance(key.slice(5, 6), JSON.parse(window.localStorage.getItem(key)), weatherData);
         }
     }
 }
 
 function checkRoadClearance(truckID, truck, weatherData) {
-    let beltItem = document.getElementById(`package${truckID}`);
-    let imageMaker = new ImageMaker();
-    console.log(localStorage.getItem(`moving${truckID}`));
     if (localStorage.getItem(`moving${truckID}`) == 'false') {
         localStorage.setItem(`moving${truckID}`, 'true');
         document.getElementById('truck'+truckID).style.border = '1px solid black';
@@ -64,10 +55,8 @@ function checkRoadClearance(truckID, truck, weatherData) {
         case 'fragile':
             if (weatherData.description === 'Rain' || weatherData.description === 'Snow') {
                 localStorage.setItem(`moving${truckID}`, 'false');
-                console.log(truckID);
                 document.getElementById('truck'+truckID).style.border = '1px solid red';
             }
         break;
     }
-    console.log(weatherData);
 }
