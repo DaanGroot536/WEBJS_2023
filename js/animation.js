cf3a49da55a78b9dfdd76a7e2da3981afc00ebce
xpetersen <ptrsnxndr@gmail.com>
2023-02-27 19:49:10 +0100

    more string literals and readability

--- js/animation.js
+++ js/animation.js
@@ -22,17 +22,17 @@ export function animate(beltcounter, imageMaker, beltItem) {
 }
 
 function move(beltcounter, imageMaker, beltItem) {
-    let node = document.getElementById("package"+beltcounter);
-    let currentPosition = parseInt(localStorage.getItem('package'+beltcounter));
+    let node = document.getElementById(`package${beltcounter}`);
+    let currentPosition = parseInt(localStorage.getItem(`package${beltcounter}`));
 
     if(currentPosition < (screen.width * 0.5) ) {
         currentPosition = currentPosition + 1;
-        localStorage.setItem('package'+beltcounter, currentPosition);
+        localStorage.setItem(`package${beltcounter}`, currentPosition);
         node.style.left = currentPosition + "px";
     }
     else {
         movePackage(beltcounter);
-        localStorage.setItem('package'+beltcounter, 0);
+        localStorage.setItem(`package${beltcounter}`, 0);
         let viewItem = document.getElementById('canvas'+beltcounter);
         viewItem.remove();
         let rnd = Math.floor(Math.random() * (4 - 0 + 1) + 0)
@@ -43,14 +43,18 @@ function move(beltcounter, imageMaker, beltItem) {
 }
 
 export function stop(beltcounter) {
-    if (document.getElementById('truck'+beltcounter).style.display === 'none') {
-        document.getElementById('package'+beltcounter).style.left = 0;
-        localStorage.setItem('package'+beltcounter, 0);
+
+    const truck = document.getElementById(`truck${beltcounter}`);
+    const item = document.getElementById(`package${beltcounter}`);
+
+    if (truck.style.display === 'none') {
+        item.style.left = 0;
         positions[beltcounter] = 0;
         clearInterval(intervals[beltcounter]);
+        localStorage.setItem(`package${beltcounter}`, 0);
     }
     else {
-        positions[beltcounter] = localStorage.getItem('package'+beltcounter);
+        positions[beltcounter] = localStorage.getItem(`package${beltcounter}`);
         clearInterval(intervals[beltcounter]);
     }
 }
