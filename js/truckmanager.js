7df4871ae55ae537b351242b47faf231f93c1bfe
xpetersen <ptrsnxndr@gmail.com>
2023-02-27 18:43:26 +0100

    merge truck manager

--- js/truckmanager.js
+++ js/truckmanager.js
@@ -5,7 +5,6 @@ export function checkTruckContent(truckID) {
     let truck = document.getElementById("truck" + truckID);
     let storedTruck = JSON.parse(localStorage.getItem(`truck${truckID}`));
     let content = truck.querySelectorAll('canvas');
-    console.log(content.length);
     let space = storedTruck.length * storedTruck.width;
     if (content.length === space) {
         truck.style.display = 'none';
@@ -26,4 +25,49 @@ function startBeltAgain(truckID) {
         animate(truckID, imageMaker, beltItem);
     }, (storedTruck.interval * 1000));
 
+}
+
+//get weather
+//compare for each truck if it can be on the road
+//args:
+//-strong wind != pallets
+
+export function checkWeather(weatherData) {
+    let trucks = [];
+
+    for (let i = 0; i < window.localStorage.length; i++) {
+        let key = window.localStorage.key(i);
+        if (key.slice(0, 5) === "truck") {
+            trucks.push(JSON.parse(window.localStorage.getItem(key)));
+            console.log(trucks);
+            checkRoadClearance(key.slice(5, 6), JSON.parse(window.localStorage.getItem(key)), weatherData);
+        }
+    }
+}
+
+function checkRoadClearance(truckID, truck, weatherData) {
+    let beltItem = document.getElementById(`package${truckID}`);
+    let imageMaker = new ImageMaker();
+    console.log(localStorage.getItem(`moving${truckID}`));
+    if (localStorage.getItem(`moving${truckID}`) == 'false') {
+        localStorage.setItem(`moving${truckID}`, 'true');
+        document.getElementById('truck'+truckID).style.border = '1px solid black';
+    }
+
+    switch (truck.type) {
+        case 'cold':
+            if (weatherData.celsius >= 35) {
+                localStorage.setItem(`moving${truckID}`, 'false');
+                document.getElementById('truck'+truckID).style.border = '1px solid red';
+            }
+        break;
+        case 'fragile':
+            if (weatherData.description === 'Rain' || weatherData.description === 'Snow') {
+                localStorage.setItem(`moving${truckID}`, 'false');
+                console.log(truckID);
+                document.getElementById('truck'+truckID).style.border = '1px solid red';
+            }
+        break;
+    }
+    console.log(weatherData);
 }
